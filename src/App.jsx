6f08f96404897b5c89569e09c7feffdfd9c4b5ce
marzia <you@example.com>
2025-07-22 16:13:46 +0200

    fix style error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <></>;
+  const url = "https://lanciweb.github.io/demo/api/actresses/";
+
+  const [api, setApi] = useState(url);
+  const [actData, setActData] = useState(null);
+
+  useEffect;
+  () => {
+    fetch(api)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setActData(data);
+      });
+  },
+    [api];
+
+  return (
+    <>
+      <p>prova</p>
+
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="card">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card’s content.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
